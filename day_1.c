af3ec5d05e9e4fc404509f46b6f4c6c07019d91c
Airus <work.airus@gmail.com>
2026-04-15 08:03:02 +0800

    Solved Greeting Loop

--- day_1.c
+++ day_1.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main() {
     char name[100];
-    char choice;
-    
+    char choice = '\0';
+    bool accepted_value = false;
+
     while (1) {
-        // Get name
-        // Print greeting
-        // Get choice
-        // Handle choice (y/n/else)
+        if (choice == 'n') {
+                printf("Goodbye!");
+                break;
+        }
+
+        printf("Hello user, may I ask what is your name?\n");
+        scanf("%s", name);
+        printf("So [%s] is your name. Interesting!\n\n", name);
+        accepted_value = false;
+
+        while (accepted_value == false) {
+                printf("Continue? (strictly y/n)");
+                scanf(" %c", &choice);
+                if (choice == 'y' || choice == 'n') {
+                        accepted_value = true;
+                        break;
+                }
+        }
     }
-    
     return 0;
 }
